0c608e14f1e5d90ed0c9d6aad3cb309b7f995249
Mariamkhalidx <mariamkhalid036@gmail.com>
2024-07-07 23:41:09 +0300

    sec commit

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 var todayRowInput=document.getElementById('todayRow');
+var nextdayRowInput=document.getElementById('nextdayRow');
 
 var weather=[];
 
@@ -9,10 +10,9 @@ async function fetchData() {
         throw new Error('Network response was not ok.');
       }
       const data = await response.json(); 
-      wheatherState=data;
+      weather=data;
       diplayTodayData();
-      console.log(wheatherState);
-
+      displayNextData();
       return data;
     } catch (error) {
       console.error('Error fetching data:', error);
@@ -23,58 +23,98 @@ fetchData();
 
 function diplayTodayData(){
 var string=``;
-for(var i=0; i<wheatherState.lenght;i++){
-  string+=`<div class="col-md-4 m-0 rounded-4 bg-light-gray ">
+let todaydate=new Date();
+console.log(weather);
+  string+=`
+  <div class="col-md-4 m-0 rounded-4 bg-light-gray ">
   <div class="d-flex justify-content-between py-2 px-3 text-white  bg-small-nav
     " id="today-date">
-    <p class="my-auto" id="today-date-name">monday</p>
+    <p class="my-auto" id="today-date-name">${todaydate.toLocaleDateString("en-US",{weekday:"long"})}</p>
     <span>
-      <span class="my-autoo" id="today-date-number">35</span>
-      <span class="my-autoo" id="today-date-month">june</span>
+      <span class="my-autoo" id="today-date-number">${todaydate.getDate()}</span>
+      <span class="my-autoo" id="today-date-month">${todaydate.toLocaleDateString("en-US",{month:"long"})}</span>
 
     </span>
 
   </div>
   <div class="text-start px-3 py-3 pt-5" id="today">
-    <p class="fs-5" id="today-location">${wheatherState[i].location.name}</p>
+    <p class="fs-5" id="today-location">${weather.location.name}</p>
     <div class="d-flex justify-content-center align-items-center">
       <h3>
       <span class="fs-1 fw-bolder">
       
-          <span id="today-temp">${wheatherState[i].current.temp_c}</span>
+          <span id="today-temp">${weather.current.temp_c}</span>
           <span><sup>o</sup>c</span>
 
       </span>
     </h3>
 
     </div>
-    <img id="today-img" src="${wheatherState[i].current.condition.icon}" alt="">
+    <img id="today-img" src="${weather.current.condition.icon}" alt="">
 
-    <p id="today-text" class="fs-5 text-primary">${wheatherState[i].current.condition.text}</p>
+    <p id="today-text" class="fs-5 text-primary">${weather.current.condition.text}</p>
 
 
   </div>
   <div class="text-light d-flex icon">
     <div class="d-flex me-4">
       <img class="me-2"  src="images/icon-umberella.png" alt="">
-      <p id="umberlla">${wheatherState[i].current.wind_mph}</p>
+      <p id="umberlla">${weather.current.humidity }</p>
 
     </div>
     <div class="d-flex me-4">
       <img class="me-2" src="images/icon-wind.png" alt="">
-      <p id="wind">${wheatherState[i].current.wind_kph}km/h</p>
+      <p id="wind">${weather.current.wind_kph}km/h</p>
       
     </div>       
      <div class="d-flex me-4">
       <img class="me-2"  src="images/icon-compass.png" alt="">
-      <p id="compass">${wheatherState[i].current.wind_dir}</p>
+      <p id="compass">${weather.current.wind_dir}</p>
 
     </div>
   </div>
-</div>`
-}
+</div>
+
+`
 todayRowInput.innerHTML=string;
+
 }
+
+function displayNextData(){
+  var string='';
+      string+=`<div class="col-md-4 m-0 rounded-4 bg-dark-gray">
+  <div class="d-flex justify-content-between py-2 px-3 text-white  bg-dark
+    " id="tom-date">
+    <p class="my-auto" id="tom-date-name">monday</p>
+
+  </div>
+  <div class="text-start px-3 py-3 pt-5" id="tom">
+    <div class="text-center">
+      <img id="tom-img" src="images/113 (1).png" alt="">
+
+      <h3 class="fs-1 fw-bolder d-block">
+        
+        <span id="tom-min-temp">${weather[i].forecastData[i+1].day.
+mintemp_c
+}</span>
+        <span><sup>o</sup>c</span>
+    </h3>
+      <span class="fs-6 fw-lighter">
+        <span id="tom-max-temp">20</span>
+        <span><sup>o</sup>c</span>
+      </span>
+      <p id="tom-text" class="fs-5 text-primary">sunny</p>
+
+    </div>
+
+
+
+  </div>
+
+</div>`
+  }
+  nextdayRowInput.innerHTML=string;
+
 
 
 
